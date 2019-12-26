afb24d04cab72c2c4b5ba5b95a1a81b9e95ef79c
Volodymyr <revuravova@gmail.com>
2019-12-26 11:33:05 +0200

    Updates

---
+++ precache-manifest.ce9f2979d0db72da8bd451daa543d918.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3621bf7833ac2a9c8e491d78cb33937",
+    "url": "/Test-social-network/index.html"
+  },
+  {
+    "revision": "a4a07701a6490db8d702",
+    "url": "/Test-social-network/static/css/2.de4a5a06.chunk.css"
+  },
+  {
+    "revision": "4e31ca37141f4b25b291",
+    "url": "/Test-social-network/static/css/main.b5a42016.chunk.css"
+  },
+  {
+    "revision": "a4a07701a6490db8d702",
+    "url": "/Test-social-network/static/js/2.ced09261.chunk.js"
+  },
+  {
+    "revision": "4e31ca37141f4b25b291",
+    "url": "/Test-social-network/static/js/main.a09cb489.chunk.js"
+  },
+  {
+    "revision": "07e659f43042bc86a202",
+    "url": "/Test-social-network/static/js/runtime~main.7517983a.js"
+  },
+  {
+    "revision": "94586fa76bd904c5520af7095244589a",
+    "url": "/Test-social-network/static/media/66363622_90839018.94586fa7.mp3"
+  },
+  {
+    "revision": "3d442e3dbcaffcad2fc8dfe5915f8a06",
+    "url": "/Test-social-network/static/media/brati-chsvv-kolyadka-1.3d442e3d.mp3"
+  },
+  {
+    "revision": "d7e43d024b773fc806f1bfffb2c5d07b",
+    "url": "/Test-social-network/static/media/brati-chsvv-r-zdvo-zoreyu-yasnoyu-s-ya.d7e43d02.mp3"
+  },
+  {
+    "revision": "4abb21c84ec8df721b43e40b2a38d7c7",
+    "url": "/Test-social-network/static/media/brati-chsvv-svyat-bog-narodivsya.4abb21c8.mp3"
+  },
+  {
+    "revision": "2779f44306eec3d91a0489ee35fdbc29",
+    "url": "/Test-social-network/static/media/brati-chsvv-svyatiy-mikolay.2779f443.mp3"
+  },
+  {
+    "revision": "4d6ae64ef60ead1b43fd8533cc827cdf",
+    "url": "/Test-social-network/static/media/brati-chsvv.4d6ae64e.mp3"
+  },
+  {
+    "revision": "dc27a61fedb6739701a66bb67c6bc822",
+    "url": "/Test-social-network/static/media/brati-monahi-chsvv-bozhe-m-y-bozhe.dc27a61f.mp3"
+  },
+  {
+    "revision": "761f9c0edd87dfbec9cc284f9027a832",
+    "url": "/Test-social-network/static/media/brati-monahi-chsvv-gorobchik.761f9c0e.mp3"
+  },
+  {
+    "revision": "0dc646ac1a0b07c477635cd0f31ffa37",
+    "url": "/Test-social-network/static/media/brati-monahi-chsvv-hristiyanin.0dc646ac.mp3"
+  },
+  {
+    "revision": "a28ae58050aafa6fa717ac246c2c9327",
+    "url": "/Test-social-network/static/media/brati-monahi-chsvv-lyubov-sila.a28ae580.mp3"
+  },
+  {
+    "revision": "ab9adbe600a771562b6bb7c55675ab19",
+    "url": "/Test-social-network/static/media/brati-monahi-chsvv-stariy-rik.ab9adbe6.mp3"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Test-social-network/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Test-social-network/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Test-social-network/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Test-social-network/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Test-social-network/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "16b67a26ce8d84e7e8ac1ebd305518c8",
+    "url": "/Test-social-network/static/media/irina-fedishin-moja-koljada.16b67a26.mp3"
+  },
+  {
+    "revision": "bc97e4076d9012dcc36a853db829917b",
+    "url": "/Test-social-network/static/media/kolyadki-na-dvor-r-zdvo.bc97e407.mp3"
+  },
+  {
+    "revision": "c8c2dedb5fdd6ecdf71f1d1a93c215f8",
+    "url": "/Test-social-network/static/media/natalka-karpa-kolyadki--tri-car.c8c2dedb.mp3"
+  },
+  {
+    "revision": "7f59a1325418691dcbc4e9e96c183866",
+    "url": "/Test-social-network/static/media/rina-fedishin-kolyadki-hay-sus-male-ditya-nove-2016.7f59a132.mp3"
+  },
+  {
+    "revision": "4b09e90d2f8c915caace05cd5a227a77",
+    "url": "/Test-social-network/static/media/ukranski_koljadki_-_nehaj_rizdvo_u_vashu_hatu_(zf.fm).4b09e90d.mp3"
+  },
+  {
+    "revision": "bba3e7c20e36ed7769693d7453938fd9",
+    "url": "/Test-social-network/static/media/Колядки рина Федишин – Україна колядує.bba3e7c2.mp3"
+  }
+]);
